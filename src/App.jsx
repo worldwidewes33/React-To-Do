fde0ee057e11363316be0b7d77f549b1151a36eb
Wesley Austin <131537918+worldwidewes33@users.noreply.github.com>
2024-03-09 12:04:18 -0700

    create and display todo on form submit

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,46 @@ import { useState } from 'react';
 
 function App() {
   const [input, setInput] = useState('');
+  const [todoItems, setTodoItems] = useState([]);
+
+  function getUniqueID() {
+    return Math.floor(Math.random() * Date.now());
+  }
 
   function handleInput(e) {
     setInput(e.target.value);
   }
 
+  function createToDo(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: getUniqueID(),
+      description: input,
+      isComplete: false
+    };
+
+    setTodoItems([...todoItems, newTodo]);
+    setInput('');
+  }
+
+  const todoList = todoItems.map((item) => {
+    return (
+      <li key={item.id}>
+        <input type="checkbox" value={item.isComplete} />
+        {item.description}
+        <button>X</button>
+      </li>
+    );
+  });
+
   return (
     <>
-      <form action="">
+      <form onSubmit={createToDo}>
         <input type="text" value={input} onChange={handleInput} />
         <input type="submit" value="Create To-Do" />
       </form>
-      <ul></ul>
+      <ul>{todoList}</ul>
     </>
   );
 }
